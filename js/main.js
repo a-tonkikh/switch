b2bcf292942d51f948a6a747647f7430102ed333
a-tonkikh <a-tonkikh@users.noreply.github.com>
2018-04-12 23:00:22 +0300

    5

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#owl-carousel").owlCarousel({
+    items: 1,
+    singleItem: true,
+    loop: true,
+    center: true,
+    nav: true,
+    navText: ["", ""],
+    dots: false,
+  });
+});
